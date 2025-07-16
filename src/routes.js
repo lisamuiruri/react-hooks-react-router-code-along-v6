ce575108af65292f957614a373b5dba17510889a
lisa <lisawanjiku41@gmail.com>
2025-07-16 08:52:47 +0300

    react-hooks-react-router-code-along-v6

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import App from "./App"; // 👈 This is the missing import
+
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+  },
+];
+
+export default routes;
